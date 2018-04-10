1a8958accb8af8da0ac81f0d0fd2fc28936aa927
furtkemmey <furtkemmey@yahoo.com.tw>
2018-04-10 18:09:22 +0800

    combine map view controller

--- testURL/ViewControllerMapView.swift
+++ testURL/ViewControllerMapView.swift
@@ -2,36 +2,123 @@
 //  ViewControllerMapView.swift
 //  testURL
 //
-//  Created by KaiChieh on 30/03/2018.
+//  Created by Qooo on 08/04/2018.
 //  Copyright © 2018 KaiChieh. All rights reserved.
 //
 
 import UIKit
+//import CoreLocation
+import MapKit
 
-class ViewControllerMapView: UIViewController {
+class ViewControllerMapView: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     var entry: Entry?
-
+    var line: String = ""
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("map view entry is \(entry?.summary)")
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print("category is \(String(describing: entry?.category))")
+        print("summary is \(String(describing: entry?.summary))")
+        
+        myMapView.delegate = self
+        
+        //將縣市過濾出來
+        if var line = entry?.summary {
+            var strSearch = "台灣"
+            switch line {
+            case let x where x.contains("縣"):
+                strSearch = "縣"
+            case let x where x.contains("市"):
+                strSearch = "市"
+            case let x where x.contains("鄉"):
+                strSearch = "鄉"
+            case let x where x.contains("線"): //鐵路
+                strSearch = "線"
+            default:
+                break
+            }
+            if strSearch != "台灣" {
+                let result = line.range(of: strSearch, options: NSString.CompareOptions.literal, range: line.startIndex..<line.endIndex, locale: nil)
+                if let range = result {
+                    let strSearchStartIndex = range.lowerBound
+                    let strSearchEndIndex = line.index(strSearchStartIndex, offsetBy: strSearch.count-3)
+                    line.removeSubrange(line.startIndex..<strSearchEndIndex)
+                    let strEnd = line.index(line.startIndex, offsetBy: 3)
+                    line.removeSubrange(strEnd..<line.endIndex)
+                    self.line = line
+                    print("KeyWord is \(line)")
+                } else { print("找不到縣市") }
+            } else { self.line = "台灣" }
+        }
+        
+        //地址轉座標
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(line, completionHandler: { placemarks, error in
+            if error != nil {
+                print(error!)
+                return
+            }
+            if let placemarks = placemarks {
+                let placemark = placemarks[0]
+                
+                //定義大頭針
+                //self.myMapView.removeAnnotation(self.myMapView.annotations as! MKAnnotation)
+                let annotation = MKPointAnnotation()
+                annotation.title = self.entry?.category
+                //annotation.subtitle = self.entry?.summary
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    //self.myMapView.showAnnotations([annotation], animated: true)
+                    self.myMapView.addAnnotation(annotation)
+                    self.myMapView.selectAnnotation(annotation, animated: true)
+                    if self.line != "台灣" {
+                        let region = MKCoordinateRegion(center: annotation.coordinate,
+                                                        span: MKCoordinateSpan(latitudeDelta: 1.5, longitudeDelta: 1.5))
+                        self.myMapView.setRegion(region, animated: false)
+                    } else {
+                        let region = MKCoordinateRegion(center: annotation.coordinate,
+                                                        span: MKCoordinateSpan(latitudeDelta: 2.5, longitudeDelta: 2.5))
+                        self.myMapView.setRegion(region, animated: false)
+                    }
+                }
+            }
+        })
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: MKMapViewDelegate 實作：自訂大頭針樣式
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil  //如果是目前位置的大頭針, 則不更動樣式
+        }
+        //用"Pin"ID來取得大頭針樣式（注意：不要轉型為預設大頭針，使用一般型的大頭針樣式）
+        var annView = mapView.dequeueReusableAnnotationView(withIdentifier: "Pin")
+        if annView == nil {
+            annView = MKAnnotationView(annotation: annotation, reuseIdentifier: "Pin")
+        }
+        
+        
+        
+        //pop左側Icon
+        //        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        //        leftIconView.image = UIImage(named: "mi")
+        //        annView?.leftCalloutAccessoryView = leftIconView
+        
+        //callout labal at detailCalloutAccessoryView
+        let label = UILabel()
+        label.numberOfLines = 10 //Label的文字行數
+        label.text = entry?.summary
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        annView?.detailCalloutAccessoryView = label
+        
+        annView?.canShowCallout = true
+        return annView //回傳樣式
     }
-    */
-
+    
+    
 }
+
+
